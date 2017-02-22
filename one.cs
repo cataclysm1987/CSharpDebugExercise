6eba0c0616b1ac2e5201a59045deebe53bc3b812
Eric Conklin <conklin.eric@gmail.com>
2017-02-22 13:14:39 -0500

    Create one.cs

---
+++ one.cs
@@ -0,0 +1,19 @@
+using System;
+using static System.Console;
+class DebugTwo1
+{
+    static void Main()
+    {
+        string name, bossName = null;
+        WriteLine("Enter your name");
+        name = ReadLine();
+        WriteLine("Hello {0}! Enter the name of your boss", name);
+        bossName = ReadLine();
+        bool areNamesTheSame = false;
+        if (name == bossName)
+        {
+            areNamesTheSame = true;
+        }
+        WriteLine("It is {0} that you are your own boss", areNamesTheSame);
+    }
+}
